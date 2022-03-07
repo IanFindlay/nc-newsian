dbeae794b099b8fc8bdfdcc8ec35ccea4be11b2c
Ian Findlay <code@findlayian.com>
2022-03-07 14:42:13 +0000

    Create Title component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Title from "./components/Title.js";
 
 function App() {
-  return <header className="App"></header>;
+  return (
+    <header className="App">
+      <Title />
+    </header>
+  );
 }
 
 export default App;
---
+++ src/components/Title.js
@@ -0,0 +1,10 @@
+export default function Title() {
+  return (
+    <section>
+      <h1>NC-Newsian</h1>
+      <h2>
+        A frontend built for an example news database using the React framework
+      </h2>
+    </section>
+  );
+}
