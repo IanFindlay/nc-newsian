56e6f27db92c2912fdac3a00f7c7924720260baa
Ian Findlay <code@findlayian.com>
2022-03-10 10:51:12 +0000

    Refactor toggleCollapsed into button onClick

--- src/components/CollapseWrapper.js
+++ src/components/CollapseWrapper.js
@@ -3,11 +3,13 @@ import { useState } from "react";
 export default function CollapseWrapper({ commentCount, children }) {
   const [isCollapsed, setIsCollapsed] = useState(true);
 
-  const toggleCollapsed = () => setIsCollapsed(!isCollapsed);
-
   return (
     <section className={"Article-comments"}>
-      <button onClick={toggleCollapsed}>
+      <button
+        onClick={() => {
+          setIsCollapsed(!isCollapsed);
+        }}
+      >
         {isCollapsed ? `Show Comments (${commentCount})` : "Hide Comments"}
       </button>
       {!isCollapsed && children}
